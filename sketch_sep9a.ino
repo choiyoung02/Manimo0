82277b25dd322f03cc9b9663834b6c1952823c40
choiyoung02 <141106891+choiyoung02@users.noreply.github.com>
2023-09-10 12:04:57 +0900

    sketch_sep9a.ino

--- sketch_sep9a.ino
+++ sketch_sep9a.ino
@@ -3,13 +3,11 @@
 #include <DallasTemperature.h>
 #include <WiFiEsp.h>
 #include <SoftwareSerial.h>
-#include <LCD5110_Graph.h>
 #include <Adafruit_NeoPixel.h>
-#include <LiquidCrystal_I2C.h>
 
 // WiFi 정보 설정
 const char ssid[] = "LGWiFi_DD04";
-const char pass[] = "---";
+const char pass[] = "-------";
 int status = WL_IDLE_STATUS;
 
 // 웹 서버 주소 및 포트 번호 설정
@@ -18,8 +16,8 @@ const int serverPort = 8080;
 WiFiEspClient client;
 
 // 핀번호 설정
-#define rxPin 3 
-#define txPin 2 
+#define rxPin 3
+#define txPin 2
 #define ONE_WIRE_BUS 4
 #define NEOPIXEL_PIN 6
 #define BUTTON_pin 7 // 버튼을 7번 핀에 연결
@@ -28,9 +26,8 @@ const int CDS_PIN = A0;
 const int TURBIDITY_PIN = A1;
 
 // 여러가지 관련 설정
-SoftwareSerial mySerial(txPin, rxPin); // 통신 관련 설정
+SoftwareSerial mySerial(txPin,rxPin); // 통신 관련 설정
 Adafruit_NeoPixel neopixels = Adafruit_NeoPixel(NEOPIXEL_NUM_LEDS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800); // 네오픽셀 관련 설정
-LiquidCrystal_I2C lcd(0x3F, 16, 2); // 테스트 lcd
 OneWire oneWire(ONE_WIRE_BUS); // DS18B20 온도센서 관련 설정
 DallasTemperature sensors(&oneWire);
 
@@ -57,35 +54,18 @@ unsigned long turbidityLogPreviousMillis = 0;
 const unsigned long temperatureChangeInterval = 60000; // milliseconds
 unsigned long temperatureChangePreviousMillis = 0;
 
-// 하루에 허용되는 최대 오류 로그 횟수
+// 하루에 허용되는 탁도 최대 오류 로그 횟수
 const int maxErrorLogsPerDay = 2;
 
 // 하루에 허용되는 최대 온도 변화 로그 횟수
 const int maxTemperatureChangeLogsPerDay = 2;
 
-// LCD5110 라이브러리 설정
-LCD5110 myLCD(8, 9, 10, 11, 12);
 
 // 조도, 탁도, 온도 임계값 설정
 const int thresholdLight = 200; // 조도 임계값
 const int thresholdTurbidity = 350; // 탁도 임계값
 const float thresholdTemperature = 30.0; // 온도 임계값
 
-// 표정 이미지 데이터
-const byte sadFace[5] = {
-  B00111100,
-  B01000010,
-  B10100101,
-  B10000001,
-  B10000001
-};
-const byte smileFace[5] = {
-  B00111100,
-  B01000010,
-  B10100101,
-  B10000001,
-  B01111110
-};
 
 // 네오픽셀 LED 색상 설정
 uint32_t sadColor = neopixels.Color(255, 0, 0); // 빨간색 (RGB)
@@ -119,10 +99,6 @@ void setup() {
     }
     neopixels.show(); // 초기에 NeoPixel 표시 활성화
   }
-  // 테스트 lcd 초기화
-  lcd.init(); // 초기화
-  lcd.backlight(); // 배경 불 켜기
-  lcd.clear();
 }
 
 void loop() {
@@ -134,28 +110,20 @@ void loop() {
   Serial.println(turbidityValue);
   // 현재 날짜 구하기
   unsigned long currentDay = currentMillis / 86400000; // 1일은 86,400,000 밀리초
-
+  // 온도 값
+  float currentTemperature = sensors.getTempCByIndex(0);
   // 표정 선택
-  bool isSad = (analogRead(CDS_PIN) < thresholdLight) || (turbidityValue > thresholdTurbidity) || (prevTemperature >= thresholdTemperature);
-
-  // 화면 지우기
-  //myLCD.clear(); 자꾸 오류뜸
+  bool isSad = (analogRead(CDS_PIN) < thresholdLight) || (turbidityValue > thresholdTurbidity) || (currentTemperature >= thresholdTemperature);
 
   // 표정 표시
   if (isSad) {
-    displayImage(sadFace);
-    lcd.setCursor(0, 0); // 테스트 lcd
-    lcd.print("state bad");
+
     // 네오픽셀 LED를 빨간색으로 설정
     for (int i = 0; i < NEOPIXEL_NUM_LEDS; i++) {
       neopixels.setPixelColor(i, sadColor);
     }
     neopixels.show();
   } else {
-    // 기본 표정 표시 (웃는 얼굴 등)
-    displayImage(smileFace);
-    lcd.setCursor(0, 0); // 테스트 lcd
-    lcd.print("state good");
     // 네오픽셀 LED를 기본 색상으로 설정 (예: 녹색)
     for (int i = 0; i < NEOPIXEL_NUM_LEDS; i++) {
       neopixels.setPixelColor(i, happyColor);
@@ -163,32 +131,12 @@ void loop() {
     neopixels.show();
   }
   delay(1000);
-  // 오류 로그를 찍을지 결정
-  bool shouldLogTurbidityError = false;
-  if (currentDay != lastLoggedDayTurb) {
-    // 새로운 날이 시작될 때, 오류 로그 횟수와 마지막 로깅된 날짜 초기화
-    turbidityErrorLogCount = 0;
-    lastLoggedDayTurb = currentDay;
-  }
-  if (turbidityErrorLogCount < maxErrorLogsPerDay) {
-    // 허용된 최대 횟수 내에서만 오류 로그를 찍음
-    shouldLogTurbidityError = true;
-  }
-
-  // 5초마다 탁도 오류 로그를 전송
-  if (currentMillis - turbidityLogPreviousMillis >= turbidityLogInterval) {
-    turbidityLogPreviousMillis = currentMillis;
-    if (shouldLogTurbidityError && (turbidityValue < 50 || turbidityValue > 900)) {
-      logTurbidityError(turbidityValue);
-      turbidityErrorLogCount++; // 오류 로그 횟수 증가
-    }
-  }
 
   // 온도 변화를 관찰하여 온도가 3도 이상 내려갈 때 로그를 보냄
   if (currentMillis - temperatureChangePreviousMillis >= temperatureChangeInterval) {
     temperatureChangePreviousMillis = currentMillis;
     sensors.requestTemperatures();
-    float currentTemperature = sensors.getTempCByIndex(0);
+    
 
     if (currentTemperature - prevTemperature >= 3) {
       if (temperatureChangeLogCount < maxTemperatureChangeLogsPerDay) {
@@ -246,19 +194,7 @@ void loop() {
   }
 }
 
-// 표정 이미지 출력 함수
-void displayImage(const byte* image) {
-  for (int row = 0; row < 5; row++) {
-    for (int col = 0; col < 8; col++) {
-      myLCD.drawBitmap(col * 8, row * 8, image, 8, 8);
-    }
-  }
-}
 
-void logTurbidityError(int value) {
-  // 탁도 오류 로그를 서버로 전송하는 코드를 작성
-  // ...
-}
 
 void logTemperatureChange(float prevTemp, float currentTemp) {
   // 온도 변화 로그를 서버로 전송하는 코드를 작성
